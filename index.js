361d18279c54faea1fe7268cd8962d39e4505be8
mehmet furkan sakiz <mehmetfurkansakiz@gmail.com>
2023-01-26 20:04:11 +0300

    JobFinder created

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./src/Pages/Jobs/Jobs";
+import { name as appName } from "./app.json";
+
+AppRegistry.registerComponent(appName, () => App);
